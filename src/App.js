f8016c5dec612272f1ff4e6fe281a95516a49761
the-duck <markspurgeon96@hotmail.com>
2018-06-08 18:04:46 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
           <p>This is the skripto website</p>
         </header>
         <p className="App-intro">
